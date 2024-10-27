ff9d09f08caf9a58cb5eb4ed1584d24c57734cc1
İzzettin Gücüm <izzettin_43@hotmail.com>
2024-10-27 17:54:33 +0300

    rf routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StripeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/product', [ProductController::class, 'displayProductListPage'])->name('display.stripe.charge.page');
+Route::get('/', [ProductController::class, 'displayProductListPage'])->name('display.stripe.charge.page');
 Route::post('/charge', [StripeController::class, 'chargePayment'])->name('stripe.charge');
 Route::get('/charge/success', [StripeController::class, 'displaySuccessPage'])->name('stripe.charge.success');
